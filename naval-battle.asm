25066cb43787c7418c6501725cdd5bd7841d4502
Pierca7 <38869168+Pierca7@users.noreply.github.com>
2020-07-19 22:06:33 -0300

    :recycle: Refactor procedure & variable names

--- naval-battle.asm
+++ naval-battle.asm
@@ -1,15 +1,14 @@
 org 100h
 
-org 100h
-jmp inicio            
-            
-disparo db "n",'$'
+jmp start
 
-botes_Jug1 db "***************",13,10
+shoot db "n",'$'
+
+player1_ships db "***************",13,10
            db "***#*****#####*",13,10
            db "***************",13,10
            db "***************",13,10
-           db "*****##********",13,10                  
+           db "*****##********",13,10
            db "***************",13,10
            db "************#**",13,10
            db "************#**",13,10
@@ -17,7 +16,7 @@ botes_Jug1 db "***************",13,10
            db "**####*********",13,10
            db "***************",13,10,'$'
 
-botes_Jug2 db "******#####****",13,10
+player2_ships db "******#####****",13,10
            db "#**************",13,10
            db "***************",13,10
            db "***********#***",13,10
@@ -28,7 +27,7 @@ botes_Jug2 db "******#####****",13,10
            db "********###****",13,10
            db "***************",13,10
            db "***************",13,10,'$'
-  
+
 board db " Player 1 Sea  || Player 2 Sea  ",13,10
       db "***************||***************",13,10
       db "***************||***************",13,10
@@ -42,258 +41,244 @@ board db " Player 1 Sea  || Player 2 Sea  ",13,10
       db "***************||***************",13,10
       db "***************||***************",13,10,
       db " Points   P1:00|| Points   P2:00",13,10,'$'
-         
-            
 
-turno db 0
+turn db 0
 
-posicion db 0
-posGrande dw 0
-puntos1x db 48,'$'
-puntos1y db 48,'$'
-puntos2x db 48,'$'            
-puntos2y db 48,'$'
+position db 0
+global_position dw 0
 
-puntos1 db 0
-puntos2 db 0
-            
+points_player1_dx db 48, '$'
+points_player1_dy db 48, '$'
+points_player2_dx db 48, '$'
+points_player2_dy db 48, '$'
 
-inicio:
-    
-    mov dx,OFFSET board 
-    mov ah,9 
-    int 21h  
-    mov dl, 0          ; Cursor X position
-    mov dh, 1          ; Cursor Y position
- 
-main:   cmp puntos1,15
+points_player1 db 0
+points_player2 db 0
+
+start:
+
+    mov dx, OFFSET board
+    mov ah, 9
+    int 21h
+    mov dl, 0                       ; Cursor X position
+    mov dh, 1                       ; Cursor Y position
+
+main:   cmp points_player1, 15
         je fin
-        cmp puntos2,15
-        je fin        
-         
-        call SetCursor  
+        cmp points_player2, 15
+        je fin
+
+        call set_cursor
         mov ah, 00h
         int 16h
-                        
-        cmp al, 120     ; Compare if the pressed key is 'x' 
+
+        cmp al, 120                 ; Compare if the pressed key is 'x'
         je Down
-                
-        cmp al, 119     ; Compare if the pressed key is 'w'    
+
+        cmp al, 119                 ; Compare if the pressed key is 'w'
         je Up
-                
-        cmp al, 97      ; Compare if the pressed key is 'a'    
+
+        cmp al, 97                  ; Compare if the pressed key is 'a'
         je Left
-                
-        cmp al, 100     ; Compare if the pressed key is 'd'   
+
+        cmp al, 100                 ; Compare if the pressed key is 'd'
         je Right
-                
-        cmp al, 115     ; Compare if the pressed key is 's', uncover if true  
+
+        cmp al, 115                 ; Compare if the pressed key is 's', uncover if true
         je uncover
-                
-        jmp main        ; If the pressed key is none of the above, ask for pressing a key again
 
-            
-;------------------- Keyboard functions --------------------------
+        jmp main                    ; If the pressed key is none of the above, ask for pressing a key again
 
-uncover:                       ; Uncover tile
-        call acerto
-        mov dx, offset disparo
+                                    ; ------------------- Keyboard functions --------------------------
+
+uncover:                            ; Uncover tile
+        call evaluate_ship_hit
+        mov dx, offset shoot
         mov ah, 9
         int 21h
-        call dibPuntos
-        call pasarTurno
-        call SetCursor      
-        jmp main  
+        call draw_points
+        call change_turn
+        call set_cursor
+        jmp main
 
-Right:  cmp dl, 14             ; Reposition cursor in X axis to the right
+Right:  cmp dl, 14                  ; Reposition cursor in X axis to the right
         je main
         cmp dl, 31
         je main
-        add dl, 1           
-        inc posicion
-        call SetCursor      
-        call calcPosGrande
+        add dl, 1
+        inc position
+        call set_cursor
+        call calculate_global_position
         jmp main
         ret
 
-Left:   cmp dl, 0              ; Reposition cursor in X axis to the left
+Left:   cmp dl, 0                   ; Reposition cursor in X axis to the left
         je main
         cmp dl, 17
         je main
-        sub dl, 1           
-        dec posicion
-        call SetCursor      
-        call calcPosGrande
+        sub dl, 1
+        dec position
+        call set_cursor
+        call calculate_global_position
         jmp main
 
         ret
 
-Up:     cmp dh, 1              ; Reposition cursor in Y axis to up
+Up:     cmp dh, 1                   ; Reposition cursor in Y axis to up
         je main
-        sub dh, 1           
-        sub posicion,17
-        call SetCursor      
-        call calcPosGrande
+        sub dh, 1
+        sub position,17
+        call set_cursor
+        call calculate_global_position
         jmp main
         ret
 
-Down:                         ; Reposition cursor in Y axis to down
+Down:                               ; Reposition cursor in Y axis to down
         cmp dh, 11
         je main
-        add dh, 1           
-        add posicion,17
-        call SetCursor      
-        call calcPosGrande
+        add dh, 1
+        add position,17
+        call set_cursor
+        call calculate_global_position
         jmp main
         ret
 
-;------------------- Functions --------------------------
+        ; ------------------- Functions --------------------------
 
-
-SetCursor proc              
+set_cursor proc
         mov ah, 02h
         mov bh, 00
-        int 10h       
+        int 10h
         ret
-SetCursor endp
+set_cursor endp
 
-pasarTurno proc    ; Change turn
-    mov posicion,0
-    cmp turno,0
+change_turn proc                     ; Change turn
+    mov position, 0
+    cmp turn, 0
     je j2
-    cmp turno,1
+    cmp turn, 1
     je j1
-    
-    j1: 
-    mov turno, 0
+
+    j1:
+    mov turn, 0
     mov dl, 0
     mov dh, 1
-    call calcPosGrande
+    call calculate_global_position
     ret
-    j2: 
-    mov turno, 1
+    j2:
+    mov turn, 1
     mov dl, 17
     mov dh, 1
-    call calcPosGrande
+    call calculate_global_position
     ret
-    
-pasarTurno endp
+change_turn endp
 
-acerto proc
-    
-    cmp board[si],"s",'$'        ; Evaluate if this position was already selected 
-    je main   
+evaluate_ship_hit proc
+
+    cmp board[si],"s",'$'           ; Evaluate if this position was already selected
+    je main
     cmp board[si],"n",'$'
     je main
-    
-    mov ax,0                     ; Move position respect the small board
-    mov al,posicion
-    mov si,ax 
-    cmp turno,0
-    je comparacion1
-    cmp turno,1
-    je comparacion2
-                          
 
-    comparacion2:
-    cmp botes_Jug1[si],"#",'$'   ; Compare if player 2 hits a target
-    je fuego
-    cmp botes_Jug1[si],"*",'$'
-    je agua 
-      
-    
-    comparacion1:
-    cmp botes_Jug2[si],"#",'$'   ; Compare if player 1 hits a target
-    je fuego
-    cmp botes_Jug2[si],"*",'$'
-    je agua       
-    
-    
-    fuego:
-    mov disparo, "s",'$'
-    mov si, posGrande      
-    mov board[si], "s",'$'  ; Re-render board adding the 's'
-    cmp turno,0
-    je suma1
-    cmp turno,1
-    je suma2
-    
-    suma1:                  ; Add points
-    inc puntos1
-    cmp puntos1,10
-    je  dib1
-    inc puntos1y
+    mov ax,0                        ; Move position respect the small board
+    mov al,position
+    mov si,ax
+    cmp turn,0
+    je comparision1
+    cmp turn,1
+    je comparision2
+
+    comparision2:
+    cmp player1_ships[si],"#",'$'   ; Compare if player 2 hits a target
+    je fire
+    cmp player1_ships[si],"*",'$'
+    je agua
+
+    comparision1:
+    cmp player2_ships[si],"#",'$'   ; Compare if player 1 hits a target
+    je fire
+    cmp player2_ships[si],"*",'$'
+    je agua
+
+    fire:
+    mov shoot, "s",'$'
+    mov si, global_position
+    mov board[si], "s",'$'          ; Re-render board adding the 's'
+    cmp turn,0
+    je add1
+    cmp turn,1
+    je add2
+
+    add1:                          ; Add points
+    inc points_player1
+    cmp points_player1,10
+    je  draw1
+    inc points_player1_y
     ret
-    dib1:
-    mov puntos1y,48,'$'
-    inc puntos1x
+    draw1:
+    mov points_player1_y,48,'$'
+    inc points_player1_dx
     ret
-    
-    suma2:
-    inc puntos2
-    cmp puntos2,10
-    je dib2
-    inc puntos2y
+
+    add2:
+    inc points_player2
+    cmp points_player2,10
+    je draw2
+    inc points_player2_dy
     ret
-    dib2:
-    sub puntos2y,48,'$' 
-    inc puntos2x
+    draw2:
+    sub points_player2_dy,48,'$'
+    inc points_player2_dx
     ret
-    
+
     agua:
-    mov disparo, "n",'$'
-    mov si, posGrande
-    mov board[si], "n",'$' ; Re-render board adding the 'n'
-    ret 
-        
-acerto endp   
-    
-proc dibPuntos ; Drag points in their positions
+    mov shoot, "n",'$'
+    mov si, global_position
+    mov board[si], "n",'$'          ; Re-render board adding the 'n'
+    ret
+evaluate_ship_hit endp
+
+proc draw_points                      ; Drag points in their positions
     mov dh,12
     mov dl,13
-    call SetCursor
-    mov dx,offset puntos1x, 
-    mov ah,9 
+    call set_cursor
+    mov dx,offset points_player1_dx,
+    mov ah,9
     int 21h
-    
+
     mov dh,12
     mov dl,14
-    call SetCursor
-    mov dx,offset puntos1y, 
-    mov ah,9 
-    int 21h 
-    
+    call set_cursor
+    mov dx,offset points_player1_y,
+    mov ah,9
+    int 21h
+
     mov dh,12
     mov dl,30
-    call SetCursor
-    mov dx,offset puntos2x, 
-    mov ah,9 
-    int 21h   
-    
+    call set_cursor
+    mov dx,offset points_player2_dx,
+    mov ah,9
+    int 21h
+
     mov dh,12
     mov dl,31
-    call SetCursor
-    mov dx,offset puntos2y, 
-    mov ah,9 
+    call set_cursor
+    mov dx,offset points_player2_dy,
+    mov ah,9
     int 21h
-    ret       
-    
-dibPuntos endp
+    ret
+draw_points endp
 
-proc calcPosGrande
+proc calculate_global_position
     mov ax,0
-    mov al,dh     ; Number of columns x 19
+    mov al,dh                       ; Number of columns x 19
     mov cl,19
     mul cl
-    add al,dl     ; Add posx
+    add al,dl                       ; Add posx
     mov si, ax
-    mov posGrande, si ; Calculate position respect the big board  
+    mov global_position, si               ; Calculate position respect the big board
     ret
-calcPosgrande endp     
+calcPosgrande endp
 
 fin:
 ret
-
-
-
-
